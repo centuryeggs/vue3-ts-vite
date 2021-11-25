3887abeb6d58f0a46e959ad6622796ba2f216b7d
cjj <junjianch@gamil.com>
2021-11-25 13:45:36 +0800

    add:引入组件

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import { defineComponent } from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
+import Comp from './components/Comp.vue'
+</script>
+
+<script lang="ts">
+export default defineComponent({
+  name: 'App'
+})
 </script>
 
 <template>
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <Comp></Comp>
 </template>
 
 <style>
---
+++ src/components/Comp.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    Comp.vue counter {{counter}}
+  </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  data() {
+    return {
+      counter: 1
+    }
+  }
+})
+</script>
+
+<style scoped>
+
+</style>
